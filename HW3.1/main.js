27bbb130951f0ee72c87ec3fd4c6e1afef7a7d7e
OlhaLapko <o.s.140594@gmail.com>
2024-11-13 23:48:16 +0200

    hw3.1

---
+++ HW3.1/main.js
@@ -0,0 +1,7 @@
+console.log(typeof undefined);
+console.log(typeof 11);
+console.log(typeof 15n);
+console.log(typeof `true`);
+console.log(typeof true);
+console.log(typeof Symbol('id'));
+console.log(typeof {});
