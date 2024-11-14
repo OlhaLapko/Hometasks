dfd08e930b4ce47cde48d0b3b2a850ce83e4119d
OlhaLapko <o.s.140594@gmail.com>
2024-11-14 13:55:27 +0200

    hw3.2+3.3

---
+++ HW3.2/main2.js
@@ -0,0 +1,4 @@
+const text1=prompt("Enter your text: ");
+const text2 = prompt("Enter your text:");
+const text3=prompt("Enter your text:");
+console.log(text1 + ' ' + text2 + ' ' + text3);
---
+++ HW3.3/main3.js
@@ -0,0 +1,2 @@
+const num = prompt("Enter your a five-digit number: ");
+console.log(num[0]+' '+num[1]+' '+num[2]+' '+num[3]+' '+num[4]);
