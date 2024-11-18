4937e90a2cb9ed14f0e835fd84b1894afa4ffd8a
OlhaLapko <o.s.140594@gmail.com>
2024-11-18 16:48:23 +0200

    hw4.2+hw4.3

--- HW4.2/main4-2.js
+++ HW4.2/main4-2.js
@@ -1,11 +1,23 @@
 const num = +prompt("Enter your 3 digits number: ");
-const n3 = num % 10;
-const n2 = Math.floor((num / 10) % 10);
-const n1 = Math.floor(num / 100);
-const numEqual = (n1 === n2) && (n2 === n3);
-console.log(numEqual);
-const digitSame = (n1 === n2) || (n2 === n3) || (n1 === n3);
-console.log(digitSame);
+
+if (!isNaN(num)) {
+    const n3 = num % 10;
+    const n2 = Math.floor((num / 10) % 10);
+    const n1 = Math.floor(num / 100);
+
+    const numEqual = (n1 === n2) && (n2 === n3);
+    console.log(numEqual);
+
+    const digitSame = (n1 === n2) || (n2 === n3) || (n1 === n3);
+    console.log(digitSame);
+} else {
+    console.log("Enter a number please")
+}
+
+
+
+
+
 
 
 
---
+++ HW4.3/main4-3.js
@@ -0,0 +1,48 @@
+const birthYear = +prompt('Введіть свій рік народження: ');
+if (isNaN(birthYear) || birthYear === null) {
+    alert('Будь ласка, введіть Ваш рік корректно')
+} else {
+    const nativeCity = prompt('Введіть місто, де Ви проживаєте: ');
+    if (nativeCity === null){
+        alert('Шкода, що Ви не захотіли ввести своє місто')
+    } else {
+        const sportActivity = prompt('Введіть улюблений вид спорту: ');
+        if (sportActivity === null){
+            alert('Шкода, що Ви не захотіли ввести свій улюблений вид спорту')
+        } else {
+            const age = 2024 - birthYear;
+
+            let cityInfo = ' ';
+            switch (nativeCity) {
+                case 'Київ':
+                    cityInfo = 'Ти живеш у столиці України';
+                    break;
+                case 'Вашингтон':
+                    cityInfo = 'Ти живеш у столиці США';
+                    break;
+                case 'Лондон':
+                    cityInfo = 'Ти живеш у столиці Великобританії';
+                    break;
+                default:
+                    cityInfo = 'Ти живеш у місті '+ nativeCity;
+            }
+
+            let sportInfo = ' ';
+            switch (sportActivity) {
+                case 'бокс':
+                    sportInfo = 'Круто! Хочеш стати як Олександр Усик?';
+                    break;
+                case 'дзюдо':
+                    sportInfo = 'Круто! Хочеш стати як Денис Вірний?';
+                    break;
+                case 'хокей':
+                    sportInfo = 'Круто! Хочеш стати як Сергій Бобровський?';
+                    break;
+                default:
+                    sportInfo = 'Твій вид спорту '+ sportActivity;
+            }
+
+            alert("Ваш вік: " + age + '. ' + cityInfo + '. ' + sportInfo)
+        }
+    }
+}
