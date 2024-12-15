e3be20a0891ba7a25eefff39a339e1b1ee7bdd3b
OlhaLapko <o.s.140594@gmail.com>
2024-12-15 23:56:42 +0200

    hw11.3

---
+++ HW11.3/main11-3.js
@@ -0,0 +1,33 @@
+const elImg = document.createElement("img");
+elImg.src = "img/1.jpg";
+elImg.id = 'anyImg';
+elImg.alt = "New image";
+elImg.style.width = "200px";
+
+document.body.appendChild(elImg);
+document.body.appendChild(document.createElement('br'));
+
+const button = document.createElement('button');
+button.id = 'btn';
+button.textContent = 'Click me';
+
+document.body.appendChild(button);
+
+const images = [
+    'img/1.jpg',
+    'img/2.jpg',
+    'img/3.jpg',
+    'img/4.jpg',
+    'img/5.jpg',
+    'img/6.jpg',
+    'img/7.jpg',
+    'img/8.jpg',
+    'img/9.jpg'
+];
+
+function showImg() {
+    const randIndex = Math.floor(Math.random() * images.length);
+    const randImg = images[randIndex];
+    elImg.src = randImg;
+}
+button.addEventListener('click', showImg);
